f327f992b5be5886fc40f01aac230432851abebc
antjamespadgett <padgettanthonyj@gmail.com>
2021-10-29 12:02:47 -0500

    did exercise

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hey welcome to my number guessing game!");
+            Console.WriteLine("See if you can guess the correct number!");
+            Console.WriteLine(" What is the upper limit for the range of the number?");
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse);
+
+
+
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess: ");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Your guess is way too high! Try again");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Your guess is way too low!! Try again");
+            }
+            else
+            {
+                Console.WriteLine("Your right on the mark!! Great guess!");
+            }
+        }
+    }
+}
